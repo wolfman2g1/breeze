4a6aa90c15caa79ad92e121bd113991fe7f3bd39
Ryan Jones <ryan.jones26@gmail.com>
2022-11-17 11:19:10 -0500

    fixed formatting

--- breeze_service/api/crud.py
+++ breeze_service/api/crud.py
@@ -6,23 +6,26 @@ from breeze_service.api import schema
 
 
 ### Customer Crud ###
-def get_customers_inner(db: Session ,limit: int = 10, search: Optional[str]= ""):
-    return  db.query(models.Customer).limit(limit).all()
+def get_customers_inner(db: Session, limit: int = 10, search: Optional[str] = ""):
+    return db.query(models.Customer).filter(models.Customer.customer_name.constraints(search).limit(limit).all())
 
-def get_customer_by_id_inner(id: uuid.UUID, db: Session ):
-    return db.query(models.Customer).filter(models.Customer.id==id).first()
 
-def get_customer_contacts_inner(id:uuid.UUID,db: Session):
-    return db.query(models.Customer).filter(models.Customer.id==id).first()
+def get_customer_by_id_inner(id: uuid.UUID, db: Session):
+    return db.query(models.Customer).filter(models.Customer.id == id).first()
+
+
+def get_customer_contacts_inner(id: uuid.UUID, db: Session):
+    return db.query(models.Customer).filter(models.Customer.id == id).first()
+
 
 def update_customer_inner(db: Session, id: uuid.UUID, update: schema.Company):
-     search = db.query(models.Customer).filter(models.Customer.id==id)
-     search.update(update.dict())
-     db.commit()
-     return search.first()
+    search = db.query(models.Customer).filter(models.Customer.id == id)
+    search.update(update.dict())
+    db.commit()
+    return search.first()
 
 
-def create_customer_inner(db: Session,  customer: schema.Company):
+def create_customer_inner(db: Session, customer: schema.Company):
     new_cust = models.Customer(**customer.dict())
     db.add(new_cust)
     db.commit()
@@ -31,13 +34,15 @@ def create_customer_inner(db: Session,  customer: schema.Company):
 
 
 """ Check if this customer already exists"""
+
+
 def get_customer_by_name(db: Session, name: str):
     return db.query(models.Customer).filter(models.Customer.customer_name == name).first()
 
+
 def delete_customer(db: Session, id: uuid.UUID):
-    customer = db.query(models.Customer).filter(models.Customer.id==id)
+    customer = db.query(models.Customer).filter(models.Customer.id == id)
     customer.delete()
     db.commit()
-
 
 #######
--- breeze_service/api/customer.py
+++ breeze_service/api/customer.py
@@ -9,24 +9,26 @@ from breeze_service.database import get_db
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/api/v1/customers" , tags=['customers'])
+router = APIRouter(prefix="/api/v1/customers", tags=['customers'])
 
 
-@router.get("/", status_code=status.HTTP_200_OK )
-def list_company(db: Session = Depends(get_db), limit: int = 10, search: Optional[str]= ""):
+@router.get("/", status_code=status.HTTP_200_OK)
+def list_company(db: Session = Depends(get_db), limit: int = 10, search: Optional[str] = ""):
     """ Returns a list of all customers """
     logger.info("Getting all Customers")
 
     return crud.get_customers_inner(db=db)
 
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schema.CompanyResponse )
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schema.CompanyResponse)
 def get_customer_by_id(id: uuid.UUID, db: Session = Depends(get_db)):
     """ Find a customer by ID"""
-    customer = crud.get_customer_by_id_inner(id=id,db=db)
+    customer = crud.get_customer_by_id_inner(id=id, db=db)
     if not customer:
         logger.error(f"Customer with {id} doesn't exist")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return customer
+
 
 @router.get('/{id}/contacts', status_code=status.HTTP_200_OK)
 def get_customer_contacts(id: uuid.UUID, db: Session = Depends(get_db)):
@@ -36,38 +38,36 @@ def get_customer_contacts(id: uuid.UUID, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return cust_contacts
 
-@router.post("/", status_code=status.HTTP_201_CREATED ,response_model=schema.Company)
-def create_company(customer: schema.Company ,db: Session = Depends(get_db)):
-    """ Create a new customer"""
-    cust_check = crud.get_customer_by_name(db,name=customer.customer_name)
-    if cust_check:
-         logger.error(f"Customer with name {customer.customer_name}, already Exists")
-         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
-    logger.info(f"Create new Customer {customer.customer_name} ")
-    return crud.create_customer_inner(db,customer=customer)
 
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schema.Company)
+def create_company(customer: schema.Company, db: Session = Depends(get_db)):
+    """ Create a new customer"""
+    cust_check = crud.get_customer_by_name(db, name=customer.customer_name)
+    if cust_check:
+        logger.error(f"Customer with name {customer.customer_name}, already Exists")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    logger.info(f"Create new Customer {customer.customer_name} ")
+    return crud.create_customer_inner(db, customer=customer)
 
 
 @router.put("/{id}", status_code=status.HTTP_200_OK, response_model=schema.Company)
 async def update_customer_by_id(id: uuid.UUID, update: schema.Company, db: Session = Depends(get_db)):
     """ Update Customer by ID"""
-    customer_query = crud.get_customer_by_id_inner(id=id,db=db)
+    customer_query = crud.get_customer_by_id_inner(id=id, db=db)
     if customer_query == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     logger.info(f"Updated: {update.customer_name}")
     return crud.update_customer_inner(db=db, id=id, update=update)
 
+
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_customer_by_id(id: uuid.UUID, db: Session = Depends(get_db)):
     """ Deletes a Customer"""
-    customer = crud.get_customer_by_id_inner(id=id,db=db)
+    customer = crud.get_customer_by_id_inner(id=id, db=db)
     if customer == None:
         logger.error()
         logger.error(f"Customer with {id} doesn't exist")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    crud.delete_customer(db=db,id=id)
+    crud.delete_customer(db=db, id=id)
     logger.info(f"Deleted Customer {customer}")
     return status.HTTP_204_NO_CONTENT
-
-
-
